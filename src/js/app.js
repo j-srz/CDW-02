aa20291550fd9dd5dd3422cc199a88eebf14caf4
Jes <suarezliceajesus@gmail.com>
2025-01-05 19:09:42 -0600

    Mejore rendimiento

--- src/js/app.js
+++ src/js/app.js
@@ -69,9 +69,13 @@ function crearGaleria() {
   const CANTIDAD_IMAGENES = 16;
 
   for (let i = 1; i <= CANTIDAD_IMAGENES; i++) {
-    const imagen = document.createElement("IMG");
-    imagen.src = `src/img/gallery/full/${i}.jpg`;
-    imagen.alt = "imagen galeria";
+    const imagen = document.createElement("PICTURE");
+	imagen.innerHTML = `
+    <source srcset="build/img/gallery/thumb/${i}.avif" type="image/avif">
+    <source srcset="build/img/gallery/thumb/${i}.webp" type="image/webp">
+    <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+	`;
+
 
     imagen.onclick = function () {
       mostrarImagen(i);
@@ -82,9 +86,12 @@ function crearGaleria() {
 }
 
 function mostrarImagen(i) {
-  const imagen = document.createElement("IMG");
-  imagen.src = `src/img/gallery/full/${i}.jpg`;
-  imagen.alt = "imagen galeria";
+	const imagen = document.createElement("PICTURE");
+	imagen.innerHTML = `
+    <source srcset="build/img/gallery/full/${i}.avif" type="image/avif">
+    <source srcset="build/img/gallery/full/${i}.webp" type="image/webp">
+    <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+	`;
 
   //Generar modal
   const modal = document.createElement("DIV");
